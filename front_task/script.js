1fc67345e0dcd9389048622e6db251b24f6da8bd
omar ibrahim <mmaro5584@gmail.com>
2026-09-23 00:30:39 +0300

    add static validation

--- front_task/script.js
+++ front_task/script.js
@@ -3,7 +3,8 @@ const SUPABASE_ANON_KEY = 'sb_publishable_jJu72RK7i5qhlDaAj8KZIQ_5VeVLHfa';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegex = /^01[0125][0-9]{8}$/;
 
 console.log("الملف اشتغل وتصل بنجاح!");
 
@@ -14,12 +15,30 @@ form.addEventListener('submit', async (e) => {
 
     e.preventDefault();
 
+const emailValue=document.getElementById('email').value;
+const phoneValue=document.getElementById('phone').value;
+
+if (!emailRegex.test(emailValue)) {
+    alert('⚠️ عذراً، صيغة البريد الإلكتروني غير صحيحة!');
+    return; 
+  }
+
+  if (!phoneRegex.test(phoneValue)) {
+    alert('⚠️ عذراً، رقم الهاتف غير صحيح (يجب أن يكون رقم مصري صحيح مكون من 11 رقماً)!');
+    return; 
+  }
+
+  
+
     const formData = {
         Fullname: document.getElementById('fullname').value,
-        Email: document.getElementById('email').value,
+        Email: emailValue,
         Password: document.getElementById('password').value,
-        Phone: document.getElementById('phone').value,
+        Phone: phoneValue
     };
+
+
+
 
     const { data, error } = await supabaseClient
         .from('Client')
@@ -30,7 +49,7 @@ form.addEventListener('submit', async (e) => {
         alert('حدث خطأ أثناء الإرسال: ' + error.message);
     } else {
         alert('تم حفظ البيانات بنجاح! 🎉');
-        form.reset(); // لتفريغ الحقول بعد الإرسال
+        form.reset(); 
     }
 
 })
